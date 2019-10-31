3abc28b9e800383c589b81b210d912bb3eb56560
Yury Delendik <ydelendik@mozilla.com>
2019-10-31 17:39:01 -0500

    eval example

---
+++ examples/eval.rs
@@ -0,0 +1,38 @@
+use failure::{bail, Error};
+use std::env;
+use std::fs;
+use std::path::Path;
+
+use wasmeval::{Instance, Module};
+
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        bail!("USAGE: eval <wasmfile> [<fn_name>]");
+    }
+    let wasmfile = &args[1];
+    let fn_name = args.get(2);
+
+    let bin = fs::read(Path::new(wasmfile)).expect("file data");
+    let module = Module::new(bin.into_boxed_slice())?;
+
+    let fn_index = fn_name.map(|name| {
+        let (index, _) = module
+            .exports()
+            .iter()
+            .enumerate()
+            .find(|(_i, e)| *e == name)
+            .expect("export");
+        index
+    });
+    let instance = Instance::new(&module, &[])?;
+    if let Some(fn_index) = fn_index {
+        let f = &instance.exports()[fn_index];
+        if let Ok(result) = f.func().unwrap().borrow().call(&[]) {
+            println!("{:?}", result);
+            return Ok(());
+        }
+        bail!("some error")
+    }
+    Ok(())
+}
