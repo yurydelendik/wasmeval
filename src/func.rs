c48ba3bce09fae0b716554e789f204817a7b8246
Yury Delendik <ydelendik@mozilla.com>
2019-11-01 08:52:13 -0500

    better cache

--- src/func.rs
+++ src/func.rs
@@ -11,6 +11,7 @@ use crate::values::{get_default_value, Trap, Val};
 pub(crate) struct InstanceFunction {
     instance_data: Rc<RefCell<InstanceData>>,
     defined_index: usize,
+    cache: RefCell<Option<InstanceFunctionBody>>,
 }
 
 impl InstanceFunction {
@@ -18,12 +19,14 @@ impl InstanceFunction {
         InstanceFunction {
             instance_data: data,
             defined_index,
+            cache: RefCell::new(None),
         }
     }
 }
 
 struct InstanceFunctionBody {
     bytecode: BytecodeCache,
+    locals: Vec<(u32, Val)>,
 }
 
 impl InstanceFunctionBody {
@@ -31,9 +34,30 @@ impl InstanceFunctionBody {
         module_data: Rc<RefCell<ModuleData>>,
         body: &wasmparser::FunctionBody<'static>,
     ) -> Self {
+        let mut locals = Vec::new();
+        for local in body.get_locals_reader().expect("reader").into_iter() {
+            let (count, ty) = local.expect("local def");
+            let local_val = get_default_value(ty.into());
+            locals.push((count, local_val));
+        }
+
         let reader = body.get_operators_reader().expect("operators reader");
         let bytecode = BytecodeCache::new(module_data, reader);
-        InstanceFunctionBody { bytecode }
+
+        InstanceFunctionBody { bytecode, locals }
+    }
+
+    pub fn create_locals(&self, params: &[Val]) -> Vec<Local> {
+        let mut locals = Vec::new();
+        for param in params {
+            locals.push(Local(param.clone()));
+        }
+        for (count, val) in self.locals.iter() {
+            for _ in 0..*count {
+                locals.push(Local(val.clone()));
+            }
+        }
+        locals
     }
 }
 
@@ -65,30 +89,24 @@ impl Func for InstanceFunction {
     }
 
     fn call(&self, params: &[Val]) -> Result<Box<[Val]>, Trap> {
-        let module_data = self.instance_data.borrow().module_data.clone();
-        let body = Ref::map(module_data.borrow(), |data| {
-            &data.func_bodies[self.defined_index]
-        });
-        let locals = read_body_locals(params, &body);
+        if self.cache.borrow().is_none() {
+            let module_data = self.instance_data.borrow().module_data.clone();
+            let body = Ref::map(module_data.borrow(), |data| {
+                &data.func_bodies[self.defined_index]
+            });
+            let module_data = self.instance_data.borrow().module_data.clone();
+            let body = InstanceFunctionBody::new(module_data, &body);
+            *self.cache.borrow_mut() = Some(body);
+        }
+        let body = self.cache.borrow();
+        let locals = body.as_ref().unwrap().create_locals(params);
         let mut ctx = EvalContext::new(self.instance_data.clone());
-        let module_data = self.instance_data.borrow().module_data.clone();
-        let body = Box::new(InstanceFunctionBody::new(module_data, &body));
-        let result = eval(&mut ctx, &*body, locals, self.results_arity());
+        let result = eval(
+            &mut ctx,
+            body.as_ref().unwrap(),
+            locals,
+            self.results_arity(),
+        );
         result
     }
-}
-
-fn read_body_locals(params: &[Val], body: &wasmparser::FunctionBody) -> Vec<Local> {
-    let mut locals = Vec::new();
-    for param in params {
-        locals.push(Local(param.clone()));
-    }
-    for local in body.get_locals_reader().expect("reader").into_iter() {
-        let (count, ty) = local.expect("local def");
-        for _ in 0..count {
-            let local_val = get_default_value(ty.into());
-            locals.push(Local(local_val));
-        }
-    }
-    locals
 }
